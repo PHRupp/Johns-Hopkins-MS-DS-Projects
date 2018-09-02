45a3ba5a89461bc8ed9f71fff96d11f8fed7d940
PHRupp <pat.h.rupp@gmail.com>
2018-09-02 15:52:50 -0500

    Completed Winnow2 Algo on Iris Data Set

--- Intro to Machine Learning/605_449_Rupp_Project_1.R
+++ Intro to Machine Learning/605_449_Rupp_Project_1.R
@@ -4,20 +4,65 @@
 # Course: 605.449 - Introduction to Machine Learning
 # Due: September 9, 2018
 
-########################### RETRIEVE DATA #############################
+#### DEFINE GLOBAL REQUIREMENTS ####
 
 # Set global environment options
 options(stringAsFactors = FALSE)
 
-# Add the source archive url to the sub sets to make full paths
-source_url <- "https://archive.ics.uci.edu/ml/datasets/"
-breast_cancer_url <- paste0(source_url, "Breast+Cancer+Wisconsin+%28Original%29")
-soybean_url <- paste0(source_url, "Soybean+%28Small%29")
-glass_url <- paste0(source_url, "Glass+Identification")
-iris_url <- paste0(source_url, "Iris")
-vote_url <- paste0(source_url, "Congressional+Voting+Records")
+#Seed makes the results reproducible
+set.seed(150)
 
-#https://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.data
+#
+
+#### RETRIEVE ALL DATA FROM WEB ####
+
+# Add the source archive url to the sub sets to make full paths
+source_url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/"
+breast_cancer_url <- paste0(source_url, "breast-cancer-wisconsin/wdbc.data")
+soybean_url <- paste0(source_url, "soybean/soybean-small.data")
+glass_url <- paste0(source_url, "glass/glass.data")
+iris_url <- paste0(source_url, "iris/iris.data")
+vote_url <- paste0(source_url, "voting-records/house-votes-84.data")
+
+#Define the columns for the iris data
+glass_cols <- c(
+  "Id"
+  , "Refractice_Index"
+  , "Sodium"
+  , "Magnesium"
+  , "Aluminum"
+  , "Silicon"
+  , "Potassium"
+  , "Calcium"
+  , "Barium"
+  , "Iron"
+  , "Type"
+)
+
+#retrieve the data from the source
+glass_set <- read.csv(
+  glass_url
+  , as.is = TRUE
+  , col.names = glass_cols
+)
+
+#Define the columns for the iris data
+iris_cols <- c(
+  "Sepal_Length"
+  , "Sepal_Width"
+  , "Petal_Length"
+  , "Petal_Width"
+  , "Class"
+)
+
+#retrieve the data from the source
+iris_set <- read.csv(
+  iris_url
+  , as.is = TRUE
+  , col.names = iris_cols
+)
+
+#Define the columns for the voter data
 vote_cols <- c(
   "Class"
   , "Handicapped_Infants"
@@ -40,10 +85,151 @@ vote_cols <- c(
 
 #retrieve the data from the source
 vote_set <- read.csv(
-  "https://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.data"
+  vote_url
   , as.is = TRUE
   , na.strings = "?"
   , col.names = vote_cols
-  )
+)
 
-############################## END CODE ###############################
+#### DEFINE WINNOW FUNCTION ####
+
+#https://en.wikipedia.org/wiki/Winnow_(algorithm)
+#http://www.cs.yale.edu/homes/aspnes/pinewiki/WinnowAlgorithm.html
+
+#Building the weights using the Winnow2 algorithm
+get_winnow2_weights <- function(
+  training_data_set
+  , cols_for_prediction #columns must have values 0 or 1
+  , col_to_pred #column must have values 0 or 1
+  , bounds = length(cols_for_prediction)
+  , weight_step = 2
+){
+  
+  #Initialize the weights
+  col_weights <- replicate( length(cols_for_prediction), 1)
+  
+  #Go through all records and change the weights for the model
+  for(row_index in 1:nrow(training_data_set)){
+    
+    #Calculate the sum of all weighted values and compare to the bound to make a prediction
+    if( sum( col_weights * training_data_set[row_index, cols_for_prediction] ) > bounds ){ 
+      prediction <- 1 
+    } else { prediction <- 0 }
+    
+    #Incorrect predictions will have their weights changed
+    if( prediction != training_data_set[row_index, col_to_pred] ){
+      
+      #Determine which values need to have their weights changed
+      cols_to_change <- which( training_data_set[row_index, cols_for_prediction] == 1 )
+      
+      #Promote weights up if 1 otherwise demote the weights
+      if( prediction == 1 ){ 
+        col_weights[cols_to_change] <- col_weights[cols_to_change] / weight_step 
+      } else { col_weights[cols_to_change] <- col_weights[cols_to_change] * weight_step }
+    }
+  }
+  
+  #Send the column weights back
+  return(col_weights)
+}
+
+#
+predict_winnow2 <- function(
+  testing_data_set
+  , cols_for_prediction #columns must have values 0 or 1
+  , col_to_pred #column must have values 0 or 1
+  , weights
+  , bounds = length(cols_for_prediction)
+){
+  #Initialize the predictions
+  predictions <- replicate( nrow(testing_data_set), NA)
+  
+  #Go through all records and change the weights for the model
+  for(row_index in 1:nrow(testing_data_set)){
+    
+    #Calculate the sum of all weighted values and compare to the bound to make a prediction
+    if( sum( col_weights * testing_data_set[row_index, cols_for_prediction] ) > bounds ){ 
+      predictions[row_index] <- 1 
+    } else { predictions[row_index] <- 0 }
+  }
+  
+  return(predictions)
+}
+
+#
+
+#### WINNOW: ANALYZE IRIS DATA ####
+
+#Analyze the data
+iris_summary <- summary(iris_set)
+
+#Set the value that we want to predict
+iris_val_to_predict <- "Iris-setosa"
+
+#Define the column to predict
+iris_set$Predict <- ifelse(iris_set$Class == iris_val_to_predict, 1, 0)
+
+#Define values for binary indicators for the predictions( using medians of numeric values )
+sepal_length_median <- as.numeric( trimws( substring(iris_summary[3,1], 9) ))
+sepal_width_median <- as.numeric( trimws( substring(iris_summary[3,2], 9) ))
+petal_length_median <- as.numeric( trimws( substring(iris_summary[3,3], 9) ))
+petal_width_median <- as.numeric( trimws( substring(iris_summary[3,4], 9) ))
+
+#Recreate the columns used for the prediction
+iris_set$Sepal_Length_calc <- ifelse(iris_set$Sepal_Length > sepal_length_median, 1, 0)
+iris_set$Sepal_Width_calc <- ifelse(iris_set$Sepal_Width > sepal_width_median, 1, 0)
+iris_set$Petal_Length_calc <- ifelse(iris_set$Petal_Length > petal_length_median, 1, 0)
+iris_set$Petal_Width_calc <- ifelse(iris_set$Petal_Width > petal_width_median, 1, 0)
+
+#Set the index for the columns to use for the prediction
+cols_for_prediction <- 7:10
+
+#Make the portion of the training set P%
+training_size <- floor(0.75 * nrow(iris_set))
+
+#define which indices will be used for the training set
+train_indices <- sample(seq_len(nrow(iris_set)), size = training_size)
+
+#Split the data into the training and testing set
+train_set <- iris_set[train_indices, ]
+test_set <- iris_set[-train_indices, ]
+
+#Calculate the models attribute weights
+iris_weights <- get_winnow2_weights(
+  training_data_set = train_set
+  , cols_for_prediction = cols_for_prediction
+  , col_to_pred = 6
+)
+
+#Use the model to predict what the values are
+test_set$Predictions <- predict_winnow2(
+  testing_data_set = test_set
+  , cols_for_prediction = cols_for_prediction
+  , col_to_pred = 6
+  , iris_weights
+)
+
+#See % of successful predictions
+length( which(test_set$Predict == test_set$Predictions) ) / nrow(test_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
